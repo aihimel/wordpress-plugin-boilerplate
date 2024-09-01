2bdd7764796d27db7160ddd634ec82a110f6dc2b
Aftabul Islam <toaihimel@gmail.com>
2024-09-01 21:04:27 +0600

    previous code removed

--- views/admin/partials/backup-time-notification.php
+++
@@ -1,8 +0,0 @@
-<div class='col-md-12'>
-	<div class="panel panel-default">
-		<div class='panel-head'>Notifications</div>
-		<div class="panel-body">
-			Basic panel example
-		</div>
-	</div>
-</div>
